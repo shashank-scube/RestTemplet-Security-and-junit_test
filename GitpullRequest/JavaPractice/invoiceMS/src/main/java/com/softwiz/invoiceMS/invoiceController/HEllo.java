3b018f0de9a7ef554187fb87c7ce675b18c8bbef
Shashank <70754401+shashank-scube@users.noreply.github.com>
2024-02-21 13:19:58 +0530

    Add files via upload
    This is commit changes

---
+++ GitpullRequest/JavaPractice/invoiceMS/src/main/java/com/softwiz/invoiceMS/invoiceController/HEllo.java
@@ -0,0 +1,5 @@
+package com.softwiz.invoiceMS.invoiceController;
+
+public class HEllo {
+
+}
---
+++ GitpullRequest/JavaPractice/invoiceMS/src/test/java/com/softwiz/invoiceMS/InvoiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwiz.invoiceMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
